a8d28476d5c2adba37e8ad053fbe3cd0f289e926
Joao Victor Nascimento <Joao8816@hotmail.com>
2024-08-23 02:24:26 -0300

    Using tkinter imc.py
    Here I'll show the new concepts that I'm learning in python, such as:
    tkinter module, using the mainloop method to keep screen up for the user, the process of catching and value using entry and then encapsulating it for future use, and etc. The growth from basic to more complex concepts it's inevitable

--- imc.py
+++ imc.py
@@ -1,33 +1,65 @@
-print("Hello, welcome to the Imc calculator")
-height = float(input("Put here your height. Example: 1.75 \n "));
-weight = float(input("Now, put yoy weight here. Example: 80 \n "));
+import tkinter as tk
 
-imc = weight / (height ** 2)
+def collect_inputs():
+    try:
+        weight = float(entry_weight.get())
+        height = float(entry_height.get())
+        imc = weight / (height ** 2)
+        
+        print(f"Your imc is: {imc:.2f}")
+        imc_element.config(text=" You're underweight")
+          
+      
+        if imc < 18.5:
+            print("You are underweight")
+            imc_element.config(text="You're underweight")
+            imc_element2.config(text="Inadequate body weight\n can result in lower energy levels,\n fatigue, and decreased\n physical and mental performance. ")
+            imc_element3.config(text="A low body weight\n can increase the risk\n of osteoporosis and fractures \ndue to insufficient bone density. ")
+        elif 18.5 <= imc < 25:
+            print("healthy, congratulations")
+            imc_element.config(text="You're healthy, keep going!")
+            imc_element2.config(text="Maintaining a healthy BMI reduces \n the risk of cardiovascular diseases\n like heart attacks and strokes.")
+            imc_element3.config(text="Maintaining a healthy BMI can lead\n to improved sleep quality\n and overall restfulness.")
+        elif 25 <= imc < 30:
+          imc_element.config(text="Overweight")
+          imc_element2.config(text=" Excess weight is a significant risk \nfactor for sleep apnea\n, a condition where\n breathing stops and starts during sleep.")
+          imc_element3.config(text="Being overweight can \n negatively impact self-esteem and \nbody image, which can affect overall mental well-being.")
+        else:
+            imc_element.config(text="You're obese")
+            imc_element2.config(text="Extra weight puts significant\n stress on joints, particularly in weight-bearing\n areas like the knees and hips, \nleading to osteoarthritis.")
+            imc_element3.config(text="Obesity can lead to poor sleep\n quality and increase the likelihood of\n sleep disturbances.")
 
 
+    except ValueError:
+        print("Invalid input. Please enter numeric values.")
 
+root = tk.Tk()
 
-print("you Imc is: " + str(round(imc)))
+root.title("Imc calculator")
+root.geometry("500x500")
 
-if imc < 18.5:
-    print("Underweight")
-    print("Inadequate body weight can result in lower energy levels, fatigue, and decreased physical and mental performance. \n")
-    print("A low body weight can increase the risk of osteoporosis and fractures due to insufficient bone density. \n")
-    print("Being underweight can lead to deficiencies in essential nutrients, which may affect your overall health and immune system.")
+text1 = tk.Label(root, text="Put here your height (Example: 1.70)", font=("Yu Gothic UI", 20))
+text1.pack(pady=20)
 
-elif 18.5 <= imc < 25:
-    print("Healthy weight")
-    print("A healthy weight is associated with a lower risk of chronic diseases such as heart disease, diabetes, and hypertension. \n")
-    print("Maintaining a healthy weight can help sustain good energy levels and overall vitality. \n")
-    print("A healthy weight supports normal bodily functions, including efficient digestion, hormonal balance, and cognitive performance. \n")
+entry_height = tk.Entry(root, font=("Yu Gothic UI", 15))
+entry_height.pack(pady=20)
 
-elif 25 <= imc < 30:
-    print("Overweight")
-    print("Being overweight increases the risk of developing chronic conditions such as type 2 diabetes, heart disease, and high blood pressure. \n")
-    print(" Overweight individuals are at higher risk for sleep apnea and other sleep-related disorders. \n")
-    print("Overweight can affect mental health, leading to issues such as low self-esteem and depression. \n")
+text2 = tk.Label(root, text="Put here your weight (Example: 80)", font=("Yu Gothic UI", 15))
+text2.pack(pady=20)
 
-else:
-    print("Obesity significantly increases the risk of developing serious health conditions such as heart disease, stroke, type 2 diabetes, and certain cancers. \n")
-    print("Excess weight can impact mobility and physical activity, leading to reduced quality of life and physical discomfort. \n")
-    print("Obesity is associated with metabolic syndrome, which includes conditions like high blood pressure, high blood sugar, and abnormal cholesterol levels. \n")
+entry_weight = tk.Entry(root, font=("Yu Gothic UI", 15))
+entry_weight.pack(pady=20)
+
+button1 = tk.Button(root, text="Submit", command=collect_inputs, font=("Yu Gothic UI", 15))
+button1.pack(pady=20)
+
+imc_element = tk.Label(root, text="", font=("Yu Gothic UI", 18))
+imc_element.pack(pady=20)
+
+imc_element2 = tk.Label(root, text="", font=("Yu Gothic UI", 18))
+imc_element2.pack(pady=20)
+
+imc_element3 = tk.Label(root, text="", font=("Yu Gothic UI", 18))
+imc_element3.pack(pady=20)
+
+root.mainloop()
